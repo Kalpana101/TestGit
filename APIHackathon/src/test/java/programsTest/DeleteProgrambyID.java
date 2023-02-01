21eedffb4cec8653a5a7a82034fd19385dedea69
Kalpana101 <kalpana.qa87@gmail.com>
2023-02-01 12:29:30 -0500

    APIhackathon commiting

---
+++ APIHackathon/src/test/java/programsTest/DeleteProgrambyID.java
@@ -0,0 +1,27 @@
+package programsTest;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class DeleteProgrambyID {
+
+	@Test
+	public void deletebyIDRequest()
+	{
+		RestAssured.baseURI ="https://lms-backend-service.herokuapp.com/lms";
+		RestAssured.basePath ="/deletebyprogid/3330";
+		
+		
+		given()
+		
+		.when()
+		.delete()
+		.then()
+		.log().all();
+		
+		
+	}
+}
